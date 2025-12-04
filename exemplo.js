64593083f176e225bab448bcc74998a799f4854a
Leri_dio <lerieverson@gmail.com>
2025-12-04 20:24:53 -0300

    segundo cominit

---
+++ exemplo.js
@@ -0,0 +1,11 @@
+alert("olá, mundo");
+const valorJantar = prompt("Valor do jantar R$: ");
+ 
+//processamento
+const valorGarson = number(valorJantar) * 0.10;
+const valorTotal = number(valorJantar) + valorGarson;
+
+//saida
+alert("Valor total: R$ " + valorTotal);
+
+
